5a814bf695f830cc918fcb626582b21195e2bc49
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-29 10:13:49 +0700

    update

--- lib/providers/transaction_provider.dart
+++ lib/providers/transaction_provider.dart
@@ -4,10 +4,11 @@ import '../models/transaction.dart';
 
 class TransactionProvider with ChangeNotifier {
   List<Transaction> transactions = [
-    Transaction(title: 'book', amount: 152000, date: DateTime.now()),
+    Transaction(title: 'Mac Studio', amount: 25200000, date: DateTime.now()),
     Transaction(
         title: 'iPhone 13 ProMax', amount: 15200000, date: DateTime.now()),
     Transaction(title: 'Macbook Air', amount: 12000000, date: DateTime.now()),
+    Transaction(title: 'Mac mini', amount: 10000000, date: DateTime.now()),
   ];
 
   List<Transaction> getTransaction() {
--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class FormScreen extends StatelessWidget {
-  const FormScreen({Key? key}) : super(key: key);
+  final formKey = GlobalKey<FormState>();
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +20,7 @@ class FormScreen extends StatelessWidget {
         ),
       ),
       body: Form(
+        key: formKey,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -25,15 +28,40 @@ class FormScreen extends StatelessWidget {
               TextFormField(
                 decoration: InputDecoration(hintText: 'Name :'),
                 autofocus: true,
+                controller: titleController,
+                validator: (String? string) {
+                  if (string!.isEmpty) {
+                    return 'ກະລຸນາຕື່ມຂໍ້ມູນ ລາຍການ';
+                  }
+                  return null;
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(hintText: 'Price :'),
                 keyboardType: TextInputType.number,
+                controller: amountController,
+                validator: (String? string) {
+                  if (string!.isEmpty) {
+                    return 'ກະລຸນາຕື່ມຂໍ້ມູນ ຈຳນວນເງິນ';
+                  }
+                  if (double.parse(string) <= 0) {
+                    return 'ກະລຸນາຕື່ມຂໍ້ມູນຫຼາຍກວ່າ ຫຼືເທົ່າກັບ 0';
+                  }
+                  return null;
+                },
               ),
               const SizedBox(height: 10),
               ElevatedButton(
                 onPressed: () {
-                  Navigator.pop(context);
+                  if (formKey.currentState!.validate()) {
+                    var title = titleController.text;
+                    var amount = amountController.text;
+
+                    print(title);
+                    print(amount);
+
+                    Navigator.pop(context);
+                  }
                 },
                 child: const Text('Add'),
                 style: ElevatedButton.styleFrom(
