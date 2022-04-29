b706358a6d01fac2f2f4145ef71c0236e2a131fb
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-29 08:50:19 +0700

    update

---
+++ lib/providers/transaction_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../models/transaction.dart';
+
+class TransactionProvider with ChangeNotifier {
+  List<Transaction> transactions = [
+    Transaction(title: 'book', amount: 152000, date: DateTime.now()),
+    Transaction(
+        title: 'iPhone 13 ProMax', amount: 152000, date: DateTime.now()),
+    Transaction(title: 'Macbook Air', amount: 152000, date: DateTime.now()),
+  ];
+
+  List<Transaction> getTransaction() {
+    return transactions;
+  }
+
+  addTransaction(Transaction statement) {
+    transactions.add(statement);
+  }
+}
