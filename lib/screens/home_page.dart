19623d636d60deedffabcdff67c4db5f8c63b58d
Hery <thitphavanh.psv@gmail.com>
2022-05-13 15:05:11 +0700

    update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_manage_database/models/transaction.dart';
 import 'package:flutter_manage_database/providers/transaction_provider.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 import 'form_screen.dart';
@@ -47,24 +48,47 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Consumer(
         builder: (context, TransactionProvider provider, Widget? child) {
-          return ListView.builder(
-            itemCount: provider.transactions.length,
-            itemBuilder: (context, int index) {
-              Transaction data = provider.transactions[index];
-              return Card(
-                margin: EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 16,
-                ),
-                elevation: 1,
-                child: ListTile(
-                  title: Text('${data.title}'),
-                  subtitle: Text('${data.date.toString()}'),
-                  trailing: Text('${data.amount.toString()}'),
-                ),
-              );
-            },
-          );
+          var count = provider.transactions.length;
+          if (count <= 0) {
+            return Center(
+              child: Text(
+                'No Data',
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+              ),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: count,
+              itemBuilder: (context, int index) {
+                Transaction data = provider.transactions[index];
+                return Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 16,
+                  ),
+                  elevation: 1,
+                  child: ListTile(
+                    title: Text(
+                      '${data.title}',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle:
+                        Text('${DateFormat("dd/MM/yyyy").format(data.date!)}'),
+                    trailing: Text(
+                      '${data.amount.toString()}',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
         },
       ),
     );
